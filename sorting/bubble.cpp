c27c41507f8d80451cbfce172d2422bff0aa46a3
Bhavesh Salvi <162792561+bhaves19h@users.noreply.github.com>
2025-09-13 22:51:15 +0530

    Create bubble.cpp

---
+++ sorting/bubble.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> bubbleSort(vector<int>& nums) {
+    int n = nums.size();
+
+    for(int i = 0 ; i<n-1 ; i++ ){
+        for(int j =  0 ; j< n-i-1 ; j++){
+            if(nums[j]>nums[j+1]){
+              int temp = arr[j];
+              arr[j] = arr[j+1];
+              arr[j+1] = arr[j];
+              int flg =1;
+            }
+
+        }
+        if(flg ==1){
+            break;
+        }
+    }
+    return nums;
+    }
+};
