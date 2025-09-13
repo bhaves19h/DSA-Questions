795f9e9ea2d3bbe327f91866b9ebb057b8109643
Bhavesh Salvi <162792561+bhaves19h@users.noreply.github.com>
2025-09-13 22:53:59 +0530

    Update bubble.cpp

--- sorting/bubble.cpp
+++ sorting/bubble.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     vector<int> bubbleSort(vector<int>& nums) {
     int n = nums.size();
-
+//time complexity  == O(n^2)
+        
     for(int i = 0 ; i<n-1 ; i++ ){
         for(int j =  0 ; j< n-i-1 ; j++){
             if(nums[j]>nums[j+1]){
