92acf632ca69a20a4bea11c9adceb7b23231b59a
Bhavesh Salvi <162792561+bhaves19h@users.noreply.github.com>
2025-09-28 18:20:41 +0530

    Create count_occurences.cpp

---
+++ Binary Search/count_occurences.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+
+    int binarySearch(vector<int>& nums, int target, bool findFirst) {
+        int n = nums.size();
+        int low = 0, high = n - 1, ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;
+                if (findFirst)
+                    high = mid - 1;  // keep searching left
+                else
+                    low = mid + 1;   // keep searching right
+            }
+            else if (nums[mid] < target) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    pair<int,int> searchRange(const vector<int>& nums, int target) {
+        int first = binarySearch(nums, target, true);
+        int last  = binarySearch(nums, target, false);
+        return {first, last};
+    }
+
+    int countOccurrences(const vector<int>& arr, int target) {
+        pair<int,int>ans = searchRange(nums, target);
+        if(ans.first == -1){
+            return 0;
+        }
+        return ans.second - ans.first + 1;
+    }
+};
