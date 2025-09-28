53297cbedc2ddb9593feea3ef88e81597c91e02d
Bhavesh Salvi <162792561+bhaves19h@users.noreply.github.com>
2025-09-29 01:12:54 +0530

    Create SIngle_element_in_sorted_array.cpp

---
+++ Binary Search/SIngle_element_in_sorted_array.cpp
@@ -0,0 +1,18 @@
+        int low = 0;
+        int high = nums.size() - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (mid % 2 == 1)
+                mid--;
+                
+            if (nums[mid] == nums[mid + 1]) {
+                low = mid + 2;
+            }
+            else {
+                high = mid;
+            }
+        }
+
+        return nums[low];
