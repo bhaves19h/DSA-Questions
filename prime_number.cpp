101b887c892341a5f4c6e2d1000e27ebac678f19
Bhavesh Salvi <162792561+bhaves19h@users.noreply.github.com>
2026-03-12 15:45:45 +0530

    Update and rename prime number to prime_number.cpp

--- prime_number.cpp
+++ prime_number.cpp
@@ -27,3 +27,30 @@ int main() {
 
     return 0;
 }
+
+
+
+
+
+
+
+#include <iostream>
+using namespace std;
+int main() {
+
+    int n =20;
+    
+    for(int i = 2 ; i<=n ; i++){
+        bool prime = true;
+    for (int j = 2; j*j <= i; j++) { 
+            if (i%j == 0 ) {
+                prime = false;
+                break;
+            }
+    }
+    if (prime) {
+        cout<<i<<endl;
+    }
+}
+    return 0;
+}
