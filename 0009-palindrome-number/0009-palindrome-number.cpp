dbef73381900070a26ef8a5e52b8eb3ae0b7dbd2
Bhavesh Salvi <162792561+bhaves19h@users.noreply.github.com>
2026-06-16 23:55:52 +0530

    Time: 0 ms (100%), Space: 8.6 MB (37.93%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -3,20 +3,14 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        // Negative numbers and numbers that end with 0 (except 0 itself) cannot be palindromes.
-        if (x < 0 /*|| (x % 10 == 0 && x != 0)*/) {
+        if (x < 0 ) {
             return false;
         }
-
         long reversed = 0;
         long original = x;
-
-        // Reverse the number with overflow check
         while (x > 0) {
             long lastDigit = x % 10;
-
-            // Check for potential overflow
-            if (reversed == INT_MAX / 10 && lastDigit > 7) {
+            if ( lastDigit > 9) {
                 return false; 
             }
             reversed = reversed * 10 + lastDigit;
