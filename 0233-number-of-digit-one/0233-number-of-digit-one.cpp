60862e750049708608c9e2ff8e024a2dd0221cfb
Bhavesh Salvi <162792561+bhaves19h@users.noreply.github.com>
2026-06-24 18:56:06 +0530

    Time: 0 ms (100%), Space: 7.8 MB (53.8%) - LeetHub

--- 0233-number-of-digit-one/0233-number-of-digit-one.cpp
+++ 0233-number-of-digit-one/0233-number-of-digit-one.cpp
@@ -3,7 +3,6 @@ public:
     int countDigitOne(int n) {
         if (n <= 0) return 0;
         if (n < 10) return 1;
-
         long power = 1;
         while (power * 10 <= n)
             power *= 10;
@@ -15,13 +14,9 @@ public:
             countDigitOne(power - 1);
 
         if (first == 1) {
-            return onesInPowerMinus1
-                 + (rest + 1)
-                 + countDigitOne(rest);
+            return onesInPowerMinus1 + (rest + 1) + countDigitOne(rest);
         } else {
-            return first * onesInPowerMinus1
-                 + power
-                 + countDigitOne(rest);
+            return first * onesInPowerMinus1+ power + countDigitOne(rest);
         }
     }
 };
