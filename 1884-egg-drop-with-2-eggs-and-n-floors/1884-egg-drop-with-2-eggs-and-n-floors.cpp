031f9d98feafa14d53493d775e1ddaa60ebf412b
Bhavesh Salvi <162792561+bhaves19h@users.noreply.github.com>
2026-07-10 01:56:50 +0530

    Time: 37 ms (20.04%), Space: 9.7 MB (25.05%) - LeetHub

---
+++ 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int solve(int k , vector<int>&dp){
+         if(k ==0 )return k;
+         if(dp[k]!= -1)return dp[k];
+        int ans = INT_MAX;
+        for(int i = 1 ; i<=k ; i++){
+            int temp = 1+max(i-1, solve(k-i,dp));
+            ans = min(ans ,temp);
+        }
+        return dp[k] =ans;
+    };
+
+    int twoEggDrop(int n) {
+        vector<int>dp(n+1 ,-1);
+
+        int res = solve(n,dp);
+        return res;
+    }
+};
