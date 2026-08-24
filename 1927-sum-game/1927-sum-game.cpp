cce355bb581eabafbedba7dad16bfb408e81cb6e
Bhavesh Salvi <162792561+bhaves19h@users.noreply.github.com>
2026-08-24 10:16:18 +0530

    Time: 4 ms (47.51%), Space: 13.8 MB (91.92%) - LeetHub

--- 1927-sum-game/1927-sum-game.cpp
+++ 1927-sum-game/1927-sum-game.cpp
@@ -1,15 +1,51 @@
 class Solution {
 public:
-    bool sumGame(string& num) {
-        const int n=num.size(), n2=n>>1;
-        int diff=0;
-        for(int i=0; i<n; i++){
-            const char c=num[i];
-            bool isq=c=='?', half=i<n2;
-            int sgn=(half<<1)-1;
-            int d=(-isq & 9)+(-!isq & (c-'0')<<1);
-            diff+=sgn*d;
+    bool sumGame(string num) {
+
+        int leftSum = 0;
+        int leftCount = 0;
+        int rightSum = 0;
+        int rightCount = 0;
+        int n = num.size();
+
+        for (int i = 0; i < n / 2; i++) {
+
+            if (num[i] == '?') {
+                leftCount++;
+            } else {
+                leftSum += (num[i] - '0');
+            }
         }
-        return diff!=0;
+
+        for (int i = n / 2; i < n; i++) {
+
+            if (num[i] == '?') {
+                rightCount++;
+            } else {
+                rightSum += (num[i] - '0');
+            }
+        }
+
+        if((leftCount + rightCount)%2)
+          return true;
+
+        int diff = leftSum - rightSum;
+
+        if(diff<0){
+            diff = -diff;
+            swap(leftCount,rightCount);
+        }
+
+        if(diff == 0){
+            return (leftCount != rightCount);
+        }else{
+            if(leftCount >= rightCount ){
+                return true;
+            }else {
+                 int bobChance = abs(leftCount - rightCount)/2;
+                 return (diff != 9*bobChance);
+            }
+        }
+      
     }
 };
